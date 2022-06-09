0000e56e97db8f8cba430447f2f462e3af6ee542
kenfry <kenfr480@gmail.com>
2022-06-09 14:28:32 +0200

    shows data properly in the list function

--- dict.py
+++ dict.py
@@ -39,8 +39,8 @@ print(help)
 while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ").lower().strip()
     if cmd == "list":
-        print('Here is the list: ')
-        print(read_dict(conn))
+        for i, wd, trans in read_dict(conn):
+            print(f"{i}: {wd} - {trans}")
     elif cmd == "add":
         word = input("  Word: ")
         translation = input("  Translation: ")
